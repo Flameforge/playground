525029e2503377a9121bb79596711e371fc208ae
The Cat <flameforge@gmail.com>
2021-08-11 00:37:58 +0200

    Add hero unit on home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,23 @@
 import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+// import logo from "./logo.svg";
+import "./App.scss";
+
+import Hero from "./components/Hero/Hero";
 
 const App: React.FunctionComponent = (): JSX.Element => {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <Hero></Hero>
+        <p>Welcome to the Cat's Playground</p>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Enter
         </a>
       </header>
     </div>
---
+++ src/components/Hero/Hero.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import "./Hero.scss";
+
+const Hero: React.FunctionComponent = (): JSX.Element => (
+  <div data-testid="Hero">
+    <div className="cat">
+      <div className="ear ear--left"></div>
+      <div className="ear ear--right"></div>
+      <div className="face">
+        <div className="eye eye--left">
+          <div className="eye-pupil"></div>
+        </div>
+        <div className="eye eye--right">
+          <div className="eye-pupil"></div>
+        </div>
+        <div className="muzzle"></div>
+      </div>
+    </div>
+  </div>
+);
+
+export default Hero;
